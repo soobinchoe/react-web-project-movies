1d1ef3ef9c3df950e5b18097c9ccb6efb296fc27
soobinchoe <65845277+soobinchoe@users.noreply.github.com>
2024-04-17 21:02:16 +0800

    feat: Add genre display in Movie component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,72 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
+    genres: []
   };
   getMovies = async () => {
-    const { data: { results } } = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=72c0aa703cf76e9067795aff01ba72a6")
+    const { data: { results } } =
+      await axios.get(
+        "https://api.themoviedb.org/3/discover/movie",
+        {
+          params: {
+            api_key: '72c0aa703cf76e9067795aff01ba72a6', // Replace with your TMDB API key
+            language: 'en-US' // Specify the language for the genre names
+          }
+        }
+      )
     this.setState({ results, isLoading: false });
+
+    // Fetch genre data from TMDB API
+    const { data: { genres } } = await axios.get(
+      "https://api.themoviedb.org/3/genre/movie/list",
+      {
+        params: {
+          api_key: '72c0aa703cf76e9067795aff01ba72a6',
+          language: 'en-US'
+        }
+      }
+    );
+    this.setState({ genres });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading, results } = this.state;
-    return <div>{isLoading
-      ? "Loading..."
-      : results.map(movie => {
-        return (
-          <Movie
-            id={movie.id}
-            year={movie.release_date}
-            title={movie.title}
-            summary={movie.overview}
-            poster={movie.poster_path} />
-        );
-      })}
-    </div>;
+    const { isLoading, results, genres } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {results.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                date={movie.release_date}
+                title={movie.title}
+                summary={movie.overview}
+                poster={movie.poster_path}
+                // Get genre names based on genre IDs
+                genres={movie.genre_ids.map(genreId => {
+                  const genre = genres.find(g => g.id === genreId);
+                  return genre ? genre.name : '';
+                })}
+              />
+            ))}
+          </div>
+        )
+        }
+      </section>
+    );
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, date, title, summary, poster }) {
-    return <h4>{title}</h4>
+function Movie({ id, date, title, summary, poster, genres }) {
+    const BASE_URL = "https://image.tmdb.org/t/p/w500/";
+    return (
+        <div className="movie">
+            <img src={BASE_URL + poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__date">{date}</h5>
+                <div className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <span key={index} className="movie__genre">
+                            {genre}{index !== genres.length - 1 && '   '}
+                        </span>
+                    ))}
+                </div>
+                <h3 className="movie__summary">{summary}</h3>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
-    date: PropTypes.string.isRequired,
+    date: PropTypes.string,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
